5ddc9cc6d3a7bd2d1a6321a20a7d255b00ad64ff
Diya Wadhwani <wadhwani2399@gmail.com>
2026-01-22 02:43:42 -0500

    updated URLs to point to BE

--- feature-flag-frontend/src/URL.ts
+++ feature-flag-frontend/src/URL.ts
@@ -1,6 +1,7 @@
-export const GRAPHQL_ENDPOINT = "http://localhost:3000/graphql";
-export const AUDIT_API_ENDPOINT = "http://localhost:3000/audit";
-export const CHECKOUT_API_ENDPOINT_V1 =
-  "http://localhost:3000/checkout/experience?version=v1";
-export const CHECKOUT_API_ENDPOINT_V2 =
-  "http://localhost:3000/checkout/experience?version=v2";
+export const BACKEND_URL =
+  import.meta.env.VITE_BACKEND_URL ?? "http://localhost:3000";
+
+export const GRAPHQL_ENDPOINT = `${BACKEND_URL}/graphql`;
+export const AUDIT_API_ENDPOINT = `${BACKEND_URL}/audit`;
+export const CHECKOUT_API_ENDPOINT_V1 = `${BACKEND_URL}/checkout/experience?version=v1`;
+export const CHECKOUT_API_ENDPOINT_V2 = `${BACKEND_URL}/checkout/experience?version=v2`;
